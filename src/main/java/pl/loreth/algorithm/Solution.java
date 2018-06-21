cdd0797de868288cbb7accda26abdbe8c77cd805
Kamil Przeniosło <kamillo_prz@gazeta.pl>
2018-06-21 20:39:39 +0200

    initialize project

---
+++ src/main/java/pl/loreth/algorithm/Solution.java
@@ -0,0 +1,9 @@
+package main.java.pl.loreth.algorithm;
+
+public class Solution {
+    public int solution(int[] R, int X, int M) {
+        int maxDistance = 0;
+
+        return maxDistance;
+    }
+}
