4680180ffef9b891f08c881547e5986e28d7f364
Kamil Przeniosło <kamillo_prz@gazeta.pl>
2018-06-26 12:59:28 +0200

    Rework from the ground

--- src/main/java/pl/loreth/algorithm/Solution.java
+++ src/main/java/pl/loreth/algorithm/Solution.java
@@ -7,101 +7,58 @@ public class Solution {
         int maxDistance;
         int boatWidth = 2 * X;
 
-        //If it is not possible to tie all the boats, the function should return
-        //happens if boats count * boat width > wharf length
+        //If it is not possible to tie all the boats, the algorithm should return
+        //happens if (boats count * boat width > wharf length)
         if (R.length * boatWidth > M) {
             return -1;
         }
 
         //initial setup of boat's bow positions, boats set next to each other starting from left
-        int boatPosition[] = new int[R.length];
-        boatPosition[0] = X;
+        int boatPositions[] = new int[R.length];
+        boatPositions[0] = X;
 
         for (int i = 1; i < R.length; i++) {
-            boatPosition[i] = boatPosition[i - 1] + boatWidth;
+            boatPositions[i] = boatPositions[i - 1] + boatWidth;
         }
 
         //initial setup of distance of mooring bollard to a corresponding boat's bow
         //distance is negative if the boat is further to the right than the bollard
-        int bollardDistance[] = new int[R.length];
+        int bollardDistances[] = new int[R.length];
 
         for (int i = 0; i < R.length; i++) {
-            bollardDistance[i] = R[i] - boatPosition[i];
+            bollardDistances[i] = R[i] - boatPositions[i];
         }
 
         //TODO: "minimal"  max distance is set by the boat with the lowest negative distance or 0 if there is none, calculate movement accordingly
 
         //if there are boats to the right of bollards in the beginning, they can be ignored, as their distance can't be improved (implicated by N bollards and N boats)
-        // - they also set the "minimal"  max distance
         int leftLimit = 0;
-        while (leftLimit < R.length && bollardDistance[leftLimit] <= 0) {
+        while (leftLimit < R.length && bollardDistances[leftLimit] <= 0) {
             leftLimit++;
         }
 
-        //minimal max distance
-        //if there is more than one bollard at the same spot, it implies minimal max distance (amount of bollards at the same spot * X)
-        maxDistance = (getMaxBollardsInOnePlace(R) - 1) * X;
-        if (leftLimit > 0) {
-            int max = getMaxDistance(bollardDistance, 0, leftLimit - 1);
-            if (max > maxDistance) {
-                maxDistance = max;
-            }
-        }
-
-        //if all boats are on the left
-        if (leftLimit == R.length) {
-            return maxDistance;
-        }
-
-        //index of boat with max distance to the right
-        int maxDistanceRightIndex = getMaxDistanceIndex(bollardDistance, leftLimit, R.length - 1);
-        int maxDistanceRight = bollardDistance[maxDistanceRightIndex];
-
-        //if minimal max distance is bigger than current max distance on the right, it is the best distance that can be achieved
-        if (maxDistance >= maxDistanceRight) {
-            return maxDistance;
-        }
-
+        int limitingDistances[] = getLimitingDistancesArray(bollardDistances);
 
         //if boat on the left doesn't exceed minimal maxDist or causes maximal distance boat to increase, there's no point in moving it, so ignore it
 //        while (leftLimit < R.length) {
-//            if (Math.abs(bollardDistance[leftLimit]) <= maxDistance) {
+//            if (Math.abs(bollardDistances[leftLimit]) <= maxDistance) {
 //                leftLimit++;
 //                //else move all unexcluded boats to the right by one
-//            } else if (Math.abs(bollardDistance[maxDistanceRightIndex] - 1) > Math.abs(bollardDistance[maxDistanceRightIndex])) {
+//            } else if (Math.abs(bollardDistances[maxDistanceRightIndex] - 1) > Math.abs(bollardDistances[maxDistanceRightIndex])) {
 //                leftLimit = maxDistanceRightIndex + 1;
 //                if (leftLimit < R.length) break;
-//                maxDistanceRightIndex = getMaxDistanceIndex(bollardDistance, leftLimit, R.length - 1);
+//                maxDistanceRightIndex = getMaxDistanceIndex(bollardDistances, leftLimit, R.length - 1);
 //            } else {
 //                //if the last boat has reached right edge of wharf, break
-//                if (R[R.length - 1] - bollardDistance[R.length - 1] + X == M) {
+//                if (R[R.length - 1] - bollardDistances[R.length - 1] + X == M) {
 //                    break;
 //                }
-//                decrementDistances(bollardDistance, leftLimit, R.length - 1);
+//                decrementDistances(bollardDistances, leftLimit, R.length - 1);
 //            }
 //        }
 
-        while (leftLimit < R.length) {
-            if (Math.abs(bollardDistance[leftLimit]) <= maxDistance) {
-                leftLimit++;
-                //else move all unexcluded boats to the right by one
-            } else if (Math.abs(bollardDistance[leftLimit] - 1) < Math.abs(bollardDistance[leftLimit + 1]) - 1) {
-                leftLimit++;
-            } else if (Math.abs(bollardDistance[maxDistanceRightIndex] - 1) >= Math.abs(bollardDistance[leftLimit] - 1)) {
-                leftLimit = maxDistanceRightIndex + 1;
-                if (leftLimit < R.length) break;
-                maxDistanceRightIndex = getMaxDistanceIndex(bollardDistance, leftLimit, R.length - 1);
-            } else {
-                //if the last boat has reached right edge of wharf, break
-                if (R[R.length - 1] - bollardDistance[R.length - 1] + X == M) {
-                    break;
-                }
-                decrementDistances(bollardDistance, leftLimit, R.length - 1);
-            }
-        }
 
-
-        return getMaxDistance(bollardDistance);
+        return getMaxDistance(bollardDistances);
     }
 
     private int getMaxDistance(int distances[]) {
@@ -125,43 +82,18 @@ public class Solution {
         return maxDistance;
     }
 
-    private int getMaxDistanceIndex(int distances[], int from, int to) {
-        int maxDistance = Math.abs(distances[from]);
-        int index = from;
-        for (; from <= to; from++) {
-            if (Math.abs(distances[from]) > maxDistance) {
-                maxDistance = Math.abs(distances[from]);
-                index = from;
+    //returns an array that specifies minimal distance (non-absolute) in a set of boats to the right of a single boat
+    private int[] getLimitingDistancesArray(int distances[]) {
+        int limitingDistances[] = new int[distances.length];
+        int lowestDistance = distances[distances.length - 1];
+        for (int i = distances.length - 1; i >= 0; i--) {
+            if (lowestDistance > distances[i]) {
+                lowestDistance = distances[i];
             }
-        }
-        return index;
-    }
-
-    //decrement all distances of a section by one
-    private void decrementDistances(int distances[], int from, int to) {
-        for (; from <= to; from++) {
-            distances[from]--;
-        }
-    }
-
-    //helper method for finding the biggest amount of bollards at the same spot
-    private int getMaxBollardsInOnePlace(int R[]) {
-        int max = 1;
-        int counter = 1;
-        int previous = R[0];
-        for (int bollard : R) {
-            if (bollard != previous) {
-                counter = 1;
-            }
-            if (max < counter) {
-                max = counter;
-            }
-            counter++;
-            previous = bollard;
+            limitingDistances[i] = lowestDistance;
         }
 
-        return max;
+        return limitingDistances;
     }
-
 
 }
